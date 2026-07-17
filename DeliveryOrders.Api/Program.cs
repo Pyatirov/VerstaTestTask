f6eb4394e368926bcdbb38c7918c476969af0d9c
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-07-17 16:33:48 +0300

    Зарегистрирован AppDbContext с SQLite и строкой подключения из конфигурации. Добавлена политика CORS для localhost. Реализован автозапуск миграций при старте.

--- DeliveryOrders.Api/Program.cs
+++ DeliveryOrders.Api/Program.cs
@@ -1,23 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+
+using DeliveryOrders.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddOpenApi();
+
+const string CorsPolicy = "AllowReactClient";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(CorsPolicy, policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
+
+app.UseCors(CorsPolicy);
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
+

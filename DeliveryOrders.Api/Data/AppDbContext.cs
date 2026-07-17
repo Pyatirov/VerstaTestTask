4f9ca4c131c884a13802d2403f2180b77838bf54
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-07-17 16:27:49 +0300

    Создан DBContext, в котором указана коллекция для "Заказов"

---
+++ DeliveryOrders.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using DeliveryOrders.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryOrders.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Order> Orders => Set<Order>();
+}

632a494df893b8999ffd1d743ee378083d18e1aa
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-07-17 16:26:15 +0300

    Создана модель сущности "Заказ"

---
+++ DeliveryOrders.Api/Models/Order.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DeliveryOrders.Api.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string SenderCity { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(300)]
+    public string SenderAddress { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string ReceiverCity { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(300)]
+    public string ReceiverAddress { get; set; } = string.Empty;
+
+    [Required]
+    [Range(0.01, 100000)]
+    public decimal Weight { get; set; }
+
+    [Required]
+    public DateTime PickupDate { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}

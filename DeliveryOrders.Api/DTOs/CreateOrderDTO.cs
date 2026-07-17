58c505df05beb0728733ead86c5b0abba27b4134
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-07-17 16:29:56 +0300

    Создан DTO для сущности "Заказ"

---
+++ DeliveryOrders.Api/DTOs/CreateOrderDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeliveryOrders.Api.DTOs;
+
+public class CreateOrderDto
+{
+    [Required]
+    [MaxLength(100)]
+    public string SenderCity { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(300)]
+    public string SenderAddress { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string ReceiverCity { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(300)]
+    public string ReceiverAddress { get; set; } = string.Empty;
+
+    [Required]
+    [Range(0.01, 100000)]
+    public decimal Weight { get; set; }
+
+    [Required]
+    public DateTime PickupDate { get; set; }
+}
